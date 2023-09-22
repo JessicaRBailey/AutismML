12000a589c3f868e59ff858ac21d05cf7b511fae
Taylor Ward <tayloresque21@gmail.com>
2023-09-22 11:32:11 -0700

    a few additional updates

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
-// Set the view engine to EJS
+// Use EJS as the view engine
 app.set('view engine', 'ejs');
 
 // Define the survey questions
 const surveyQuestions = [
-  'What is your name?',
-  'How old are you?',
-  'What is your favorite color?'
+  'Does your child look at you when you call his/her name?',
+  'How easy is it for you to get eye contact with your child?',
+  'Does your child point to indicate that s/he wants something?',
+  'Does your child point to share interest with you? (e.g. a toy that is out of reach)',
+  'Does your child pretend? (e.g. care for dolls, talk on a toy phone)',
+  'Does your child follow where you are looking?',
+  'If you or someone else in the family is visibly upset, does your child show signs of wanting to comfort them?',
+  'Would you describe your child\'s first words as:',
+  'Does your child use simple gestures? (e.g. wave goodbye)',
+  'Does your child stare at nothing with no apparent purpose?'
 ];
 
 // Define the survey options
@@ -25,15 +32,14 @@ app.get('/', (req, res) => {
 });
 
 app.post('/submit', (req, res) => {
-  const responses = surveyQuestions.map((question) => req.body[question]);
-  const selectedOption = req.body.selectedOption;
+  const surveyData = {};
 
-  // Store responses and selected option in a JSON file
-  const surveyData = {
-    responses,
-    selectedOption
-  };
+  surveyQuestions.forEach((question) => {
+    const selectedOption = req.body[`selectedOption_${question}`];
+    surveyData[question] = { selectedOption };
+  });
 
+  // Store surveyData in a JSON file
   fs.readFile('survey_responses.json', (err, data) => {
     let responsesArray = [];
     if (!err) {
@@ -51,3 +57,6 @@ app.post('/submit', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+
